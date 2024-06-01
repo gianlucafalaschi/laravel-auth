d490b5a6103f3511194c8e1bde57796e8a2d02c8
gianlucafalaschi <gian100gian@outlook.it>
2024-06-01 17:27:29 +0200

    validation in update and error message in edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -11,14 +11,26 @@
           <label for="name" class="form-label">Projects Name</label>
           <input type="text" class="form-control" id="name" name="name" value="{{ ($project->name) }}">
         </div>
+        @error('name')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <div class="mb-3">
           <label for="client_name" class="form-label">Client name</label>
           <input type="text" class="form-control" id="client_name" name="client_name" value="{{ ($project->client_name) }}">
         </div>
+        @error('client_name')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <div class="mb-3">
             <label for="summary" class="form-label">Summary</label>
             <textarea class="form-control" id="summary" rows="3" name="summary">{{ ($project->summary) }}</textarea>
         </div>
+        @error('summary')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>  
 @endsection
